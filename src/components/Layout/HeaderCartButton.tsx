ffc9e85467936cf66b2b39217b70dc93625ab224
jiyunnoh <ianjy1127@gmail.com>
2022-04-14 15:26:33 -0500

    143. Using the Context

--- src/components/Layout/HeaderCartButton.tsx
+++ src/components/Layout/HeaderCartButton.tsx
@@ -1,14 +1,20 @@
-import React, { MouseEventHandler } from "react";
+import React, { MouseEventHandler, useContext } from "react";
+import CartContext from "../../store/cart-context";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props: { onClick: MouseEventHandler }) => {
+    const cartCtx = useContext(CartContext);
+
+    const numberOfCartItmes = cartCtx.items.reduce((curNumber, item: any) => {
+        return curNumber + item.amount
+    }, 0);
     return <button className={classes.button} onClick={props.onClick}>
         <span className={classes.icon}>
             <CartIcon />
         </span>
         <span>Your Cart</span>
-        <span className={classes.badge}>3</span>
+        <span className={classes.badge}>{numberOfCartItmes}</span>
     </button>
 };
 
